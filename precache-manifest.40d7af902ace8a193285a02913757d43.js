1d4cb8abbf0898038bb0de0afd8b5493b11ea780
Ashu <ashu1652002@gmail.com>
2022-09-14 19:34:21 +0530

    Updates

---
+++ precache-manifest.40d7af902ace8a193285a02913757d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d861827cbb7fb4f9d0e6",
+    "url": "/ProfessionalWebsite/static/css/main.0a344954.chunk.css"
+  },
+  {
+    "revision": "d861827cbb7fb4f9d0e6",
+    "url": "/ProfessionalWebsite/static/js/main.d861827c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/ProfessionalWebsite/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/ProfessionalWebsite/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "f92664e5fc841460025d",
+    "url": "/ProfessionalWebsite/static/js/runtime~main.f92664e5.js"
+  },
+  {
+    "revision": "ea3e8c826ac1d8e773923ddf1342e464",
+    "url": "/ProfessionalWebsite/index.html"
+  }
+];
