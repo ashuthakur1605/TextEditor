970af6fa66eaa95632d74dfbca19e91c36bc63e6
Ashu <ashu1652002@gmail.com>
2022-11-25 17:02:11 +0530

    All done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-// import './App.css';
+// import React from "react";
+// // import './App.css';
+// import { useState } from 'react';
+// import Web from './practice/web';
+// import Nav from './practice/Naav';
+// import './practice/T.css';
+// // import Aboutus from './practice/About'; 
+
+// function App() {
+//   const [mode, setmode] = useState('light')
+
+//   const toggleMode = () => {
+//     if (mode === 'light') {
+//       setmode('dark');
+//       document.body.style.backgroundColor = '#003344'
+//     }
+//     else {
+//       setmode('light');
+//       document.body.style.backgroundColor = 'white'
+//     }
+//   }
+
+//   return (<>
+
+//     <Nav title='TextUtils' mode={mode} toggleMode={toggleMode} />
+//     <Web mode={mode} />
+//   </>
+
+//   );
+// }
+
+// export default App;
+
 import { useState } from 'react';
 import Web from './practice/web';
 import Nav from './practice/Naav';
 import './practice/T.css';
+import Aboutus from './practice/About';
 // import Aboutus from './practice/About'; 
 
 import React from "react";
-
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
 
@@ -19,20 +54,46 @@ function App() {
     if (mode === 'light') {
       setmode('dark');
       document.body.style.backgroundColor = '#003344'
+      document.title = 'TextUtils - Dark Mode'
     }
     else {
       setmode('light');
       document.body.style.backgroundColor = 'white'
+      document.title = 'TextUtils - Light Mode'
     }
   }
-  
+
+  // function App() {
+  //   document.body.style.backgroundColor = 'white'
+  // }
+
+
+
   return (<>
 
-    <Nav title='TextUtils' mode={mode} toggleMode={toggleMode} />
-    <Web mode={mode} />
-  </>
+    <Router>
+      <Nav title='TextUtils' mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
 
-  );
+        <Switch>
+
+          <Route exact path="/about">
+            <Aboutus />
+          </Route>
+
+          <Route exact path="/">
+            <Web mode={mode} />
+          </Route>
+        </Switch>
+      </div>
+
+      {/* <Nav/> */}
+      {/* <Aboutus/> */}
+    </Router>
+
+  {/* <Nav title='TextUtils' mode={mode} toggleMode={toggleMode} />
+  <Web mode={mode}  */}
+    </>
+);
 }
-
 export default App;
--- src/practice/Naav.js
+++ src/practice/Naav.js
@@ -1,50 +1,26 @@
 
 import React from "react"
+import { Link } from "react-router-dom"
 // import { a } from "react-router-dom"
 // import PropType from 'prop-types'
 export default function Nav(props) {
     return (
         <>
             <header>
-                {/* <nav className={`navbar navbar-expand-lg navbar-
-                ${props.mode} bg-${props.mode}`}>
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
-                                </li>
-                                
-                               
-                            </ul>
-                            <form className="d-flex" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                    <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </div>
-                </nav> */}
                 <nav className={`navbar navbar-expand-lg navbar-
                 ${props.mode} bg-${props.mode}`}>
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">{props.title}</a>
+                        <Link className="navbar-brand"  style={{color: props.mode === 'dark' ? 'white' : 'black' }} to="/">{props.title}</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav">
                                <li>
-                                <a className="nav-a active" aria-current="page" href="#">Home</a>
+                               <Link className="btn btn-primary mx-1" to='/' role="button">Home</Link>
                                </li>
                                 <li>
-                                <a className="nav-a" href="#">About</a>
+                                <Link className="btn btn-primary mx-1" to='/about' role="button">About</Link>
                                 </li>
                             </div>
                         </div>
